2d624af75cbe30340c9bde958c13579d884b86b1
Valentin Kruglikov <dev.n@bk.ru>
2023-04-20 12:56:23 +0700

    Fix mutable callback function and create fabric

--- src/useCallback.ts
+++ src/useCallback.ts
@@ -1,14 +1,23 @@
-import {useCallback as _useCallback, DependencyList} from 'react';
+import {useMemo as _useMemo, DependencyList} from 'react';
 
 const $IS_USE_CALLBACK_FUNCTION_SYMBOL = Symbol('IS_RELY_ON_USE_CALLBACK_FUNCTION');
 
-export interface UseCallbackFunction<T extends Function> {
+export interface UseCallbackFunction<T extends (...args: any[]) => any > {
+    (...args: Parameters<T>) : ReturnType<T>;
     [$IS_USE_CALLBACK_FUNCTION_SYMBOL]: true
 }
 
-export const useCallback = <T extends Function>(callback: T, deps: DependencyList): UseCallbackFunction<T> => {
-    const _callback = _useCallback(callback, deps);
-    _callback[$IS_USE_CALLBACK_FUNCTION_SYMBOL] = true;
+/**
+ * Ignore using `extends Function`
+ * https://github.com/DefinitelyTyped/DefinitelyTyped/issues/52873#issuecomment-845806435
+ */
+export const useCallback = <T extends (...args: any[]) => any>(callback: T, deps: DependencyList): UseCallbackFunction<T> =>
+    _useMemo(() => {
+        const newFn = (...args: Parameters<T>) => callback(...args);
+        Object.defineProperty(newFn, $IS_USE_CALLBACK_FUNCTION_SYMBOL, { value: true });
+        if (callback.name) {
+            Object.defineProperty(newFn, 'name', { value: `rely(${callback.name})` });
+        }
 
-    return _callback as unknown as UseCallbackFunction<T>
-}
+        return newFn as UseCallbackFunction<T>;
+    }, deps);
